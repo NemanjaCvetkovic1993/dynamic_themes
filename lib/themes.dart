04e11de3e4a4bb3277b0076e634729fd83d8f4a7
Nemanja Cvetkovic <44754161+NemanjaCvetkovic1993@users.noreply.github.com>
2020-09-28 14:42:38 +0200

    Version 1.0.0

--- lib/themes.dart
+++ lib/themes.dart
@@ -1,33 +1,114 @@
 import 'package:flutter/material.dart';
 
-enum MyThemeKeys { LIGHT, DARK, DARKER }
+enum MyThemeKeys { PURPLE, YELLOW, GREEN, RED, BLUE, BROWN, PINK, ORANGE }
 
 class MyThemes {
-  static final ThemeData lightTheme = ThemeData(
-    primaryColor: Colors.blue,
-    brightness: Brightness.light,
-  );
-
-  static final ThemeData darkTheme = ThemeData(
-    primaryColor: Colors.grey,
+  static final ThemeData purpleTheme = ThemeData(
+    primaryColor: Colors.purple[700],
     brightness: Brightness.dark,
   );
-
-  static final ThemeData darkerTheme = ThemeData(
-    primaryColor: Colors.black,
+  static final ThemeData yellowTheme = ThemeData(
+    primaryColor: Colors.yellow[700],
+    brightness: Brightness.dark,
+  );
+  static final ThemeData greenTheme = ThemeData(
+    primaryColor: Colors.green[800],
+    brightness: Brightness.dark,
+  );
+  static final ThemeData redTheme = ThemeData(
+    primaryColor: Colors.red[800],
+    brightness: Brightness.dark,
+  );
+  static final ThemeData blueTheme = ThemeData(
+    primaryColor: Colors.blue[800],
+    brightness: Brightness.dark,
+  );
+  static final ThemeData brownTheme = ThemeData(
+    primaryColor: Colors.brown[600],
+    brightness: Brightness.dark,
+  );
+  static final ThemeData pinkTheme = ThemeData(
+    primaryColor: Colors.pink[700],
+    brightness: Brightness.dark,
+  );
+  static final ThemeData orangeTheme = ThemeData(
+    primaryColor: Colors.orange[800],
     brightness: Brightness.dark,
   );
 
   static ThemeData getThemeFromKey(MyThemeKeys themeKey) {
     switch (themeKey) {
-      case MyThemeKeys.LIGHT:
-        return lightTheme;
-      case MyThemeKeys.DARK:
-        return darkTheme;
-      case MyThemeKeys.DARKER:
-        return darkerTheme;
+      case MyThemeKeys.PURPLE:
+        return purpleTheme;
+      case MyThemeKeys.YELLOW:
+        return yellowTheme;
+      case MyThemeKeys.GREEN:
+        return greenTheme;
+      case MyThemeKeys.RED:
+        return redTheme;
+      case MyThemeKeys.BLUE:
+        return blueTheme;
+      case MyThemeKeys.BROWN:
+        return brownTheme;
+      case MyThemeKeys.PINK:
+        return pinkTheme;
+      case MyThemeKeys.ORANGE:
+        return orangeTheme;
       default:
-        return lightTheme;
+        return purpleTheme;
+    }
+  }
+
+  static stringToEnum(themeEnumValue) {
+    switch (themeEnumValue) {
+      case "Purple":
+        return MyThemeKeys.PURPLE;
+      case "Yellow":
+        return MyThemeKeys.YELLOW;
+      case "Green":
+        return MyThemeKeys.GREEN;
+      case "Red":
+        return MyThemeKeys.RED;
+      case "Blue":
+        return MyThemeKeys.BLUE;
+      case "Brown":
+        return MyThemeKeys.BROWN;
+      case "Pink":
+        return MyThemeKeys.PINK;
+      case "Orange":
+        return MyThemeKeys.ORANGE;
+      default:
+        return MyThemeKeys.PURPLE;
+    }
+  }
+
+  static enumToString(MyThemeKeys themeKey) {
+    switch (themeKey) {
+      case MyThemeKeys.PURPLE:
+        return "Purple";
+      case MyThemeKeys.YELLOW:
+        return "Yellow";
+      case MyThemeKeys.GREEN:
+        return "Green";
+      case MyThemeKeys.RED:
+        return "Red";
+      case MyThemeKeys.BLUE:
+        return "Blue";
+      case MyThemeKeys.BROWN:
+        return "Brown";
+      case MyThemeKeys.PINK:
+        return "Pink";
+      case MyThemeKeys.ORANGE:
+        return "Orange";
+      default:
+        return "Purple";
     }
   }
 }
+
+// Convert Enum to string
+String colorPurple = MyThemeKeys.PURPLE.toString();
+
+// Convert to enum
+MyThemeKeys f = MyThemeKeys.values
+    .firstWhere((e) => e.toString() == 'Fruit.' + colorPurple);
