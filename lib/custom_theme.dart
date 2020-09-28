e7c3f32924e8bc003278ab3404a18fa937c9a098
Nemanja Cvetkovic <44754161+NemanjaCvetkovic1993@users.noreply.github.com>
2020-09-28 14:47:39 +0200

    Changed changeTheme( ) and made getTheme( ). Tryed initState.

--- lib/custom_theme.dart
+++ lib/custom_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:dynamic_themes/themes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:songapp/settings/themes.dart';
 
 class _CustomTheme extends InheritedWidget {
   final CustomThemeState data;
@@ -45,21 +46,35 @@ class CustomTheme extends StatefulWidget {
 
 class CustomThemeState extends State<CustomTheme> {
   ThemeData _theme;
+  String _themeState;
 
   ThemeData get theme => _theme;
 
   @override
   void initState() {
     _theme = MyThemes.getThemeFromKey(widget.initialThemeKey);
+
+    // Here I was trying to make an If Else statemene, to getTheme() first, and if it's a null, to get initialThemeKey.
+
+    // _theme = if(MyThemes.stringToEnum(getTheme()) != null ) MyThemes.stringToEnum(getTheme()); else {MyThemes.getThemeFromKey(widget
+    //     .initialThemeKey); }
+
     super.initState();
   }
 
-  
-
-  void changeTheme(MyThemeKeys themeKey) {
+  void changeTheme(MyThemeKeys themeKey) async {
     setState(() {
       _theme = MyThemes.getThemeFromKey(themeKey);
+      _themeState = MyThemes.enumToString(themeKey);
     });
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    await _prefs.setString("stringThemeValue", _themeState);
+  }
+
+  Future<String> getTheme() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    String themeValue = _prefs.getString("stringThemeValue") ?? "Purple";
+    return themeValue;
   }
 
   @override
